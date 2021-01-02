c46ee8548bb3b0f4101e1310cd818726e91f4d2c
Richard Henry Rozario <richardrozario.rr@gmail.com>
2021-01-03 00:04:44 +0530

    img

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+
+import logo from "../../assets/images/logo.png";
 
 const index = () => {
-    return (
-        <>
-            Navbar
-        </>
-    )
-}
+  return (
+    /* Navbar */
+    <div className="navbar">
+      <div className="container flex">
+        <img src={logo} alt="strides" />
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="features.html">Features</a>
+            </li>
+            <li>
+              <a href="docs.html">Docs</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
 
-export default index
+export default index;
