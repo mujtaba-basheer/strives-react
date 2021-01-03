2223c7bd5ad05d2455f06d715fdd983cfd72ba02
XC3000 <richardrozario.rr@gmail.com>
2021-01-03 12:43:29 +0530

    sass structure and navbar inital push

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -8,6 +8,7 @@ const index = () => {
     <>
       <Navbar />
       <Showcase />
+      <Showcase />
     </>
   );
 };
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,24 +3,68 @@ import React from "react";
 import logo from "../../assets/images/logo.png";
 
 const index = () => {
+  /*const changeNavbar = () => {
+    var navbar = document.getElementsByClassName("navbar")[0];
+    var sticky = navbar.offsetTop;
+    console.log(sticky, window.pageYOffset);
+    if (window.pageYOffset === sticky) {
+        console.log("true");
+        navbar.classList.remove("sticky");
+    } else {
+        console.log("false");
+        navbar.classList.add("sticky");
+
+    }
+  };
+
+  window.addEventListener("scroll", changeNavbar); */
+
   return (
     /* Navbar */
-    <div className="navbar">
-      <div className="container flex">
-        <img src={logo} alt="strides" />
+    <div className="navbar flex">
+      <div className="main-nav flex">
+        <img className="logo" src={logo} alt="strides" />
         <nav>
-          <ul>
+          <ul className="flex">
             <li>
               <a href="/">Home</a>
             </li>
             <li>
-              <a href="features.html">Features</a>
+              <a href="">Shop</a>
             </li>
             <li>
-              <a href="docs.html">Docs</a>
+              <a href="">Brands</a>
+            </li>
+            <li>
+              <a href="">Collections</a>
+            </li>
+            <li>
+              <a href="">Discover</a>
+            </li>
+            <li>
+              <a href="">Ethnic</a>
             </li>
           </ul>
         </nav>
+      </div>
+      <div className="extra-options">
+        <ul className="flex">
+          <li>
+            <a href="#">
+              <i className="far fa-user-circle"></i>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <i className="fas fa-heart"></i>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <i className="fas fa-shopping-cart"></i>
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
