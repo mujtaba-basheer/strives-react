e07843a4a92db9ca419fa5ab759ee22837a29be2
XC3000 <richardrozario.rr@gmail.com>
2021-01-17 21:22:42 +0530

    user sidebar ui

--- src/components/Navbar/Sidebar/Account.js
+++ src/components/Navbar/Sidebar/Account.js
@@ -1,67 +1,57 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Account = () => {
   useEffect(() => {
     const accordianItemHeaders = document.querySelectorAll(
       ".accordian-item-header"
     );
+    /* console.log(accordianItemHeaders); */
     accordianItemHeaders.forEach((accordianItemHeader) => {
-      accordianItemHeader.addEventListener("click", (event) => {
-        console.log("clicked");
+      accordianItemHeader.addEventListener("click", () => {
+        /* console.log("clicked"); */
         accordianItemHeader.classList.toggle("active");
       });
     });
   });
 
   return (
-    <div className="accordian">
-      <div className="accordian-item">
-        <div className="accordian-item-header active">Home</div>
-        <div className="accordian-item-body">
-          <ul className="flex">
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-          </ul>
-        </div>
-      </div>
-      <div className="accordian-item">
-        <div className="accordian-item-header">Account Settings</div>
-        <div className="accordian-item-body">
-          <ul className="flex">
-            <li>Profile Information</li>
-            <li>Manage Address</li>
-            <li>Change Password</li>
-          </ul>
-        </div>
-      </div>
-      <div className="accordian-item">
-        <div className="accordian-item-header">Brands</div>
-        <div className="accordian-item-body">
-          <ul className="flex">
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-          </ul>
-        </div>
-      </div>
-      <div className="accordian-item">
-        <div className="accordian-item-header">Collections</div>
-        <div className="accordian-item-body">
-          <ul className="flex">
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <>
+      <ul className="sidebar-account">
+        <li>
+          <Link>My Orders</Link>
+        </li>
+
+        <li>
+          <div className="accordian">
+            <div className="accordian-item">
+              <div className="accordian-item-header">Account Settings</div>
+              <div className="accordian-item-body">
+                <ul className="flex">
+                  <li>
+                    <Link to="/my-account">Profile Information</Link>
+                  </li>
+                  <li>
+                    <Link to="/manage-address">Manage Address</Link>
+                  </li>
+                  <li>
+                    <Link to="/change-password">Change Password</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+
+        <li>
+          <Link>My Stuff</Link>
+        </li>
+
+        <li>
+          <Link>Logout</Link>
+        </li>
+      </ul>
+    </>
   );
 };
 
