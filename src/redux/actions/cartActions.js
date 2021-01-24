c3127cb9a935cad41ae53bf2051b1710d8ea0e84
Mujtaba Basheer <mujtaba.fleapo@gmail.com>
2021-01-24 16:08:01 +0530

    Added cart_shipping_address reducer and action

--- src/redux/actions/cartActions.js
+++ src/redux/actions/cartActions.js
@@ -1,59 +1,78 @@
-import axios from "axios";
+import apiCall from "../../utils/apiCall";
 import {
-    CART_ADD_ITEM,
-    CART_REMOVE_ITEM,
-    CART_SAVE_SHIPPING_ADDRESS,
-    CART_SAVE_PAYMENT_METHOD,
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
+  CART_SHIPPING_ADDRESS_FAIL,
 } from "../constants/cartConstants";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-    const { data } = await axios.get(`/api/products/${id}`);
-    const { _id, name, image, price, countInStock } = data;
+  const { data } = await apiCall.get(`/api/products/${id}`);
+  const { _id, name, image, price, countInStock } = data;
 
-    dispatch({
-        type: CART_ADD_ITEM,
-        payload: {
-            product: _id,
-            name,
-            image,
-            price,
-            countInStock,
-            qty,
-        },
-    });
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: _id,
+      name,
+      image,
+      price,
+      countInStock,
+      qty,
+    },
+  });
 
-    localStorage.setItem(
-        "cartItems",
-        JSON.stringify(getState().cart.cartItems)
-    );
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
 
 export const removeFromCart = (id) => async (dispatch, getState) => {
-    dispatch({
-        type: CART_REMOVE_ITEM,
-        payload: id,
-    });
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  });
 
-    localStorage.setItem(
-        "cartItems",
-        JSON.stringify(getState().cart.cartItems)
-    );
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const getShippingAddress = (data) => async (dispatch, getState) => {
+  const {
+    userLogin: { userInfo },
+  } = getState();
+
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${userInfo.token}`,
+    },
+  };
+
+  try {
+    const {
+      data: { data },
+    } = await apiCall.get("get-address", config);
+
+    dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data });
+  } catch (error) {
+    if (process.env.NODE_ENV === "development") console.error(error);
+    dispatch({ type: CART_SHIPPING_ADDRESS_FAIL });
+  }
 };
 
 export const saveShippingAddress = (data) => (dispatch) => {
-    dispatch({
-        type: CART_SAVE_SHIPPING_ADDRESS,
-        payload: data,
-    });
+  dispatch({
+    type: CART_SAVE_SHIPPING_ADDRESS,
+    payload: data,
+  });
 
-    localStorage.setItem("shippingAddress", JSON.stringify(data));
+  localStorage.setItem("shippingAddress", JSON.stringify(data));
 };
 
 export const savePaymentMethod = (data) => (dispatch) => {
-    dispatch({
-        type: CART_SAVE_PAYMENT_METHOD,
-        payload: data,
-    });
+  dispatch({
+    type: CART_SAVE_PAYMENT_METHOD,
+    payload: data,
+  });
 
-    localStorage.setItem("paymentMethod", JSON.stringify(data));
+  localStorage.setItem("paymentMethod", JSON.stringify(data));
 };
