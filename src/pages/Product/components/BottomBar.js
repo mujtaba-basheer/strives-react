c8aa9d96d6d22af1c1219d054675f697577204e0
Richard Henry Rozario <richardrozario.rr@gmail.com>
2021-02-11 01:59:19 +0530

    all product two column addeed

--- src/pages/Product/components/BottomBar.js
+++ src/pages/Product/components/BottomBar.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const BottomBar = () => {
-    return (
-        <div className="allproducts__bottombar">
-            <div className="sort flex">Sort</div>
-            <div className="filter flex">Filter</div>
-        </div>
-    )
-}
+  return (
+    <div className="allproducts__bottombar">
+      <div className="sort flex">Sort</div>
+      <div className="filter flex">Filter</div>
+    </div>
+  );
+};
 
-export default BottomBar
+export default BottomBar;
