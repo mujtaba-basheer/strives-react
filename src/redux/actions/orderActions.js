e72c0009ffeb72dcaadda5095f37e5c31d0c0552
Mujtaba Basheer <mujtaba.fleapo@gmail.com>
2021-02-20 23:25:46 +0530

    Removed old function to place order

--- src/redux/actions/orderActions.js
+++ src/redux/actions/orderActions.js
@@ -52,35 +52,6 @@ export const checkCoupon = (code, amount) => async (dispatch, getState) => {
   }
 };
 
-export const createOrder = (order) => async (dispatch, getState) => {
-  try {
-    dispatch({ type: ORDER_CREATE_REQUEST });
-
-    const {
-      userLogin: { userInfo },
-    } = getState();
-
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${userInfo.token}`,
-      },
-    };
-
-    const { data } = await apiCall.post("/api/orders", order, config);
-
-    dispatch({ type: ORDER_CREATE_SUCCESS, payload: data });
-  } catch (error) {
-    dispatch({
-      type: ORDER_CREATE_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    });
-  }
-};
-
 export const getOrderDetails = (id) => async (dispatch, getState) => {
   try {
     dispatch({ type: ORDER_DETAILS_REQUEST });
@@ -181,7 +152,7 @@ export const payOrder = (amount, order = {}) => async (dispatch, getState) => {
           type: ORDER_PAY_SUCCESS,
           payload: { ...response, receipt },
         });
-        dispatch(createOrder(order));
+        dispatch(placeOrder(order));
       }
     };
 
