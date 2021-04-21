da2aad93c6a0bbd0ead481bed04542183e5b6063
Richard Henry Rozario <richardrozario.rr@gmail.com>
2021-04-21 22:27:58 +0530

    added pages

---
+++ src/pages/Extra/PrivacyPolicy.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Footer from "../../components/Navbar";
+import Footer from "../../components/Footer";
+
+const PrivacyPolicy = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
+
+export default PrivacyPolicy;
